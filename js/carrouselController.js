2086f1a36b0e2edf5b0fa67ba21173b26af5c2d0
Juan Ignacio Gonzalez Nadin <juani.87@gmail.com>
2016-09-18 15:06:42 -0300

    creating basic framework

---
+++ js/carrouselController.js
@@ -0,0 +1,12 @@
+
+var CarouselController = function() {
+	var carousel = new ch.Carousel(ch('.demo-carousel')[0], {
+	    pagination: true
+	});
+	console.log("carousel on");
+};
+
+
+$(document).on('ready', function(){
+	carouselCtrl = new CarouselController();
+});
