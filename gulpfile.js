54dbfa85144283673f732db75460769e0941e9d1
Juan Ignacio Gonzalez Nadin <juani.87@gmail.com>
2016-09-18 17:55:29 -0300

    reorganizing...

--- gulpfile.js
+++ gulpfile.js
@@ -14,36 +14,36 @@ gulp.task('default', ['html', 'sass', 'js']);
 
 // js Task
 gulp.task('js', function () {
-	gulp.src('js/*.js')
+	gulp.src('src/js/*.js')
 	.pipe(concat('script.min.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('js/min/'));
+	.pipe(gulp.dest('public/js/'));
 });
 
 
 // html Task
 gulp.task('html', function() {
-	return gulp.src(['*.html','!index.html'])
+	return gulp.src(['src/*.html','!index.html'])
 	.pipe(concat('index.html'))
 	.pipe(htmlmin({collapseWhitespace: true}))
-	.pipe(gulp.dest('./'));
+	.pipe(gulp.dest('public/'));
 });
 
 // sass Tasks (sass + autoprefixer + minify)
 gulp.task('sass', function () {
-	gulp.src('./sass/**/*.scss')
+	gulp.src('src/sass/**/*.scss')
 	.pipe(sass().on('error', sass.logError))
 	.pipe(autoprefixer())
-	.pipe(gulp.dest('./css'))
+	.pipe(gulp.dest('src/css'))
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(concat('styles.min.css'))
-	.pipe(gulp.dest('css/min'));
+	.pipe(gulp.dest('public/css/'));
 });
 
 // css Task (deprecated)
 gulp.task('css', function() {
-	return gulp.src('css/*.css')
+	return gulp.src('src/css/*.css')
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(concat('styles.min.css'))
-	.pipe(gulp.dest('css/min'));
+	.pipe(gulp.dest('public/css/'));
 });
